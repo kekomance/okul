bba700df7b0000d04e0e0746f3e135e668ec2590
kekomance <kesikyazilim@gmail.com>
2017-07-12 18:36:43 +0300

    ako

--- ogrencianasayfa.php
+++ ogrencianasayfa.php
@@ -1,8 +1,40 @@
+
+<?php
+require("vtbaglan.php");
+
+	session_start();
+	$ogrenci_id=$_SESSION["ogrenci"];
+	
+
+$sorgu=$db->query("SELECT * from ogrenciler WHERE ogrenci_id=$ogrenci_id", PDO::FETCH_ASSOC);
+foreach ($sorgu as $yaz ) 
+{  $ogrenci_adi=$yaz["ogrenci_adi"] ; 
+   $ogrenci_soyadi=$yaz["ogrenci_soyadi"];
+
+
+}
+
+	
+if(!(isset($_SESSION["ogrenci"])))
+	{
+		    	Echo "<script>window.location='index.php';</script>"; 
+		
+		exit;
+	}	
+	
+
+
+?>
+
+
+
 <html>
+<head><meta charset="utf-8"></head>
+ 
  <br>
 <table border=1 align="center" width="1000" height="650">
 
-<tr><td width="100" height="150">ÖĞRENCİ FOTOSU</td>  <td width="667"> BANNER GELECEK</td></tr>
+<tr><td width="100" height="150">ÖĞRENCİ FOTOSU</td>  <td width="667" align="center"> <div align="left"> <?php  echo"$ogrenci_adi"; echo"<br>"; echo"$ogrenci_soyadi";       ?></div>BANNER GELECEK</td> <td width="50"><a href="oturumkapat.php">ÇIKIŞ</a></td></tr>
 
 <tr>
 <td width="100" ><table border=1 height="550"> 
@@ -14,7 +46,7 @@
 <tr><td> DERS PROGRAMI </td> </tr>
 <tr><td> SINAV TARİHLERİ </td> </tr>
 </table> 
-<td > <h1> İÇERİK KISMI </h1></td></td>
+<td colspan="2"> <h1> İÇERİK KISMI </h1></td></td>
 </tr>
 </table>
 
@@ -22,18 +54,3 @@
 
 
 
-<?php
-require("vtbaglan.php");
-
-
-	session_start();
-	if(!(isset($_SESSION["b"])))
-	{
-		    	Echo "<script>window.location='index.php';</script>"; 
-		
-		exit;
-	}	
-
-
-
-?>
